203cb5b0748304b878f8ca0a21cb9585c737643e
Adem Yıldırım <yldrmadem52@gmail.com>
2024-01-14 15:49:04 +0300

    First Commit

---
+++ Core/UdemyCarBook.Domain/Entities/Feature.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Domain.Entities
+{
+    public class Feature
+    {
+        public int FeatureID { get; set; }
+        public int FeatureID { get; set; }
+    }
+}
